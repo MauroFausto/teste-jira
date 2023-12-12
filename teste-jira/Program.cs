6eb343edb610c15d95792d042e5fb334a26b217d
mauro_fausto <89610280+MauroFausto@users.noreply.github.com>
2023-12-12 15:25:09 -0300

    commit inicial
    commit inicial

---
+++ teste-jira/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace teste_jira
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
